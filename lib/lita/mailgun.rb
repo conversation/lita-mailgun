b20c1894b3a836de23f508eebedd6ed53e7851ea
James Healy <james@yob.id.au>
2016-06-12 23:31:46 +1000

    Stop treating mailgun webhooks as JSON
    ... they're not

--- lib/lita/mailgun.rb
+++ lib/lita/mailgun.rb
@@ -11,9 +11,7 @@ module Lita
 
       def mailgun_event(request, response)
         body = request.body.read
-        event = MailgunEvent.new(body)
-        robot.trigger(:mailgun_event, event: event)
-        robot.send_message(target, pretty_json(body))
+        robot.send_message(target, body)
       end
 
       private
@@ -21,13 +19,6 @@ module Lita
       def target
         Source.new(room: Lita::Room.find_by_name("tcbot-testing") || "general")
       end
-
-			def pretty_json(serialised_json_data)
-				deserialised_data = JSON.parse(serialised_json_data)
-				JSON.pretty_generate(deserialised_data)
-			rescue JSON::ParserError
-				'{"error":"Unable to parse data"}'
-			end
 
     end
 
